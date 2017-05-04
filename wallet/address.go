d673704dbd3d7b8905ba14271890b70910f8ce51
jjy <jjy@hykeep.com>
2017-05-04 16:47:30 +0800

    modified:   wallet/address.go

--- wallet/address.go
+++ wallet/address.go
@@ -11,7 +11,7 @@ type ValidateAddress struct {
 }
 
 type Address struct {
-	Result float64 `json:"result"`
+	Result string `json:"result"`
 	Error  interface{} `json:"error"`
 	ID     int `json:"id"`
 }
